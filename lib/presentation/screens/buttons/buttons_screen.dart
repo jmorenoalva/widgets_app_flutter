f99634eb6b9a018a442ba8a177d1403f8a379efc
Javier Moreno <jmoreno@sintad.pe>
2025-01-23 18:23:12 -0500

    Clase 127. Boton personalizado (button screen finished)

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -62,10 +62,10 @@ class _ButtonsView extends StatelessWidget {
             TextButton.icon(
               onPressed: (){},
               icon: const Icon(Icons.account_box_outlined),
-              label: const Text('Text')
+              label: const Text('Text Icon')
             ),
 
-            //TODO: custom button
+            const CustomButton(),
 
 
             IconButton(onPressed: (){}, icon: const Icon(Icons.app_registration_rounded)),
@@ -80,6 +80,32 @@ class _ButtonsView extends StatelessWidget {
             ),
 
           ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap:(){},
+          child: const Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola Mundo', style: TextStyle(color: Colors.white))
+          ),
         ),
       ),
     );
