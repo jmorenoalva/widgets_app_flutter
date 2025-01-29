ff591605095443595f9ddff9076e209576702549
Javier Moreno <jmoreno@sintad.pe>
2025-01-29 18:40:40 -0500

    Clase 138. Snackbars

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -32,12 +32,18 @@ const appMenuItems = <MenuItem>[
   ),
 
   MenuItem(
-    title: 'ProgressIndicators',
+    title: 'Progress Indicators',
     subTitle: 'Generales y controlados',
     link: '/progress',
     icon: Icons.refresh_rounded,
   ),
 
+  MenuItem(
+    title: 'Snackbars y dialogos',
+    subTitle: 'Indicadores en pantalla',
+    link: '/snackbars',
+    icon: Icons.info_outline,
+  ),
 
 ];
 
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -31,5 +31,11 @@ final appRouter = GoRouter(
       name: ProgressScreen.name,
       builder: (context, state) => const ProgressScreen(),
     ),
+
+    GoRoute(
+      path: '/snackbars',
+      name: SnackbarScreen.name,
+      builder: (context, state) => const SnackbarScreen(),
+    ),
   ],
 );
